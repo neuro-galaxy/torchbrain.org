f37ba65f98bdc2f2cee0a94bdc74841bf9b91e38
Vinam Arora <vinam@seas.upenn.edu>
2025-08-24 20:26:01 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ function App() {
     <div>
       {/* Header */}
       <div
-        className="header py-6 border-b-gray-400"
-        style={{ borderBottomWidth: "1px" }}
+        className="header py-6 border-b-gray-600"
+        style={{ borderBottomWidth: "0.5px" }}
       >
         <div className="mx-auto" style={{ maxWidth: "1550px" }}>
           <div className="flex justify-between">
-            <div className="flex items-center gap-4">
-              <div className="w-8 h-8 bg-white"></div>
-              <h1 className="text-3xl font-bold text-white">torch brain</h1>
+            <div className="flex items-center gap-2">
+              <div className="w-6 h-6 bg-white"></div>
+              <h1 className="text-xl font-bold text-white">torch brain</h1>
             </div>
 
-            <div className="flex items-center gap-12 text-white text-lg">
-              <a>Packages</a>
-              <a>Docs</a>
+            <div className="flex items-center gap-12 text-white font-medium">
+              <a href="test">Packages</a>
+              <a href="test">Docs</a>
             </div>
           </div>
         </div>
