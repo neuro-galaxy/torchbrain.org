9c559e3e9d127198e1e4eabe0384ea3f3c22d387
Vinam Arora <vinam@posteo.de>
2025-08-24 22:10:04 -0400

    header dropdowns

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,62 @@
+interface DropdownItem {
+  label: string;
+  href: string;
+}
+
+interface DropdownProps {
+  label: string;
+  items: DropdownItem[];
+}
+
+function Dropdown({ label, items }: DropdownProps) {
+  return (
+    <div className="relative inline-block group">
+      <button className="group-hover:opacity-80">{label}</button>
+      <div className="absolute z-10 right-0 invisible group-hover:visible">
+        <div className="mt-4 bg-slate-600 border-slate-900 rounded-sm">
+          {items.map((item, index) => (
+            <a
+              key={index}
+              href={item.href}
+              className="block px-4 py-2 hover:bg-slate-700"
+            >
+              <div className="flex items-center gap-2">
+                <div className="w-10 h-10 bg-white"></div>
+                <span>{item.label}</span>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Header() {
+  const packagesItems = [
+    {
+      label: "torch_brain",
+      href: "https://github.com/neuro-galaxy/torch_brain/",
+    },
+    { label: "brainsets", href: "https://github.com/neuro-galaxy/brainsets/" },
+    {
+      label: "temporaldata",
+      href: "https://github.com/neuro-galaxy/temporaldata/",
+    },
+  ];
+
+  const docsItems = [
+    {
+      label: "torch_brain",
+      href: "https://github.com/neuro-galaxy/torch_brain/",
+    },
+    { label: "brainsets", href: "https://github.com/neuro-galaxy/brainsets/" },
+    {
+      label: "temporaldata",
+      href: "https://github.com/neuro-galaxy/temporaldata/",
+    },
+  ];
+
   return (
     <div
       className="header py-6 border-b-gray-600"
@@ -12,8 +70,8 @@ function Header() {
           </div>
 
           <div className="flex items-center gap-12 font-medium">
-            <a href="test">Packages</a>
-            <a href="test">Docs</a>
+            <Dropdown label="Packages" items={packagesItems} />
+            <Dropdown label="Docs" items={docsItems} />
           </div>
         </div>
       </div>
