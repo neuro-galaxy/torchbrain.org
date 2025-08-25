1c6ddb6d8d7887e3d7196e52bd4be3ec7f625103
Vinam Arora <vinam@posteo.de>
2025-08-24 21:27:43 -0400

    package cards

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,37 @@ function App() {
         <br />
         Supercharged.
       </div>
+
+      <div className="flex justify-between max-w-[1000px] mx-auto">
+        <PackageCard
+          title="torch_brain"
+          description="DL pipeline building blocks and models."
+        />
+        <PackageCard
+          title="brainsets"
+          description="A collection of neural datasets"
+        />
+        <PackageCard
+          title="temporaldata"
+          description="High-performance neural data format"
+        />
+      </div>
+    </div>
+  );
+}
+
+function PackageCard({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-40 h-40 bg-white"></div>
+      <span className="mt-2 font-mono font-bold text-lg">{title}</span>
+      <span>{description}</span>
     </div>
   );
 }
