209a1f854701ee9a20917e30a1f82a8d7ad58d8b
Vinam Arora <vinam@posteo.de>
2025-08-24 21:38:28 -0400

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,7 @@
-import Header from "./components/header";
+import Landing from "./pages/Landing";
 
 function App() {
-  return (
-    <div>
-      <Header />
-
-      <div className="text-center font-extrabold text-5xl py-24 custom-headline">
-        Deep Learning on Neural Data,
-        <br />
-        Supercharged.
-      </div>
-
-      <div className="flex justify-between max-w-[1000px] mx-auto">
-        <PackageCard
-          title="torch_brain"
-          description="DL pipeline building blocks and models."
-        />
-        <PackageCard
-          title="brainsets"
-          description="A collection of neural datasets"
-        />
-        <PackageCard
-          title="temporaldata"
-          description="High-performance neural data format"
-        />
-      </div>
-    </div>
-  );
-}
-
-function PackageCard({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) {
-  return (
-    <div className="flex flex-col items-center">
-      <div className="w-40 h-40 bg-white"></div>
-      <span className="mt-4 font-mono font-bold text-xl">{title}</span>
-      <span className="mt-1">{description}</span>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
