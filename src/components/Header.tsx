633b1ddead520e7ae2c7886ae4b0178d5f99c74f
Vinam Arora <vinam@posteo.de>
2025-08-24 22:22:30 -0400

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ function Dropdown({ label, items }: DropdownProps) {
     <div className="relative inline-block group">
       <button className="group-hover:opacity-80">{label}</button>
       <div className="absolute z-10 right-0 invisible group-hover:visible">
-        <div className="mt-4 bg-slate-600 border-slate-900 rounded-sm">
+        <div className="mt-2 py-2 bg-slate-800 border-slate-900 rounded-sm">
           {items.map((item, index) => (
             <a
               key={index}
@@ -22,7 +22,7 @@ function Dropdown({ label, items }: DropdownProps) {
             >
               <div className="flex items-center gap-2">
                 <div className="w-10 h-10 bg-white"></div>
-                <span>{item.label}</span>
+                <span className="font-mono text-xs font-normal">{item.label}</span>
               </div>
             </a>
           ))}
@@ -48,12 +48,12 @@ function Header() {
   const docsItems = [
     {
       label: "torch_brain",
-      href: "https://github.com/neuro-galaxy/torch_brain/",
+      href: "https://torch-brain.readthedocs.io/en/latest/",
     },
-    { label: "brainsets", href: "https://github.com/neuro-galaxy/brainsets/" },
+    { label: "brainsets", href: "https://brainsets.readthedocs.io/en/latest/" },
     {
       label: "temporaldata",
-      href: "https://github.com/neuro-galaxy/temporaldata/",
+      href: "https://temporaldata.readthedocs.io/en/latest/",
     },
   ];
 
