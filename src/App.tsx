23f2e4ffaf6fd9f8d69c91bf80c79f1b3c1334cb
Vinam Arora <vinam@seas.upenn.edu>
2025-08-24 20:21:03 -0400

    new website yo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      {/* Header */}
+      <div
+        className="header py-6 border-b-gray-400"
+        style={{ borderBottomWidth: "1px" }}
+      >
+        <div className="mx-auto" style={{ maxWidth: "1550px" }}>
+          <div className="flex justify-between">
+            <div className="flex items-center gap-4">
+              <div className="w-8 h-8 bg-white"></div>
+              <h1 className="text-3xl font-bold text-white">torch brain</h1>
+            </div>
+
+            <div className="flex items-center gap-12 text-white text-lg">
+              <a>Packages</a>
+              <a>Docs</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
